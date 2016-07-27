9978b1350c0c48c46bc358ed82cdf3c85fab95a6
Leandro Matos <leandromatos@is-uz.com>
2016-07-27 13:08:50 -0300

    Improves the condition to show the link to edit team

--- resources/views/team/show.blade.php
+++ resources/views/team/show.blade.php
@@ -4,6 +4,6 @@
 <h3>Owner: {{ $team->owner()->lists('name') }}</h3>
 <h3>Members: {{ implode(', ', $team->members()->pluck('name')->all()) }}</h3>
 
-@if(auth()->check() && (Gate::check('edit_team') || $team->isOwnedBy(auth()->user())))
+@if(auth()->check() && (auth()->user()->can('edit_team') || $team->isOwnedBy(auth()->user())))
     <h3><a href="{{ route('team.edit', $team) }}">Edit team</a></h3>
 @endif
